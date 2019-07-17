6e73626c63e5530d20144adc6346675ef2bffbcf
Twilight <twilivez@gmail.com>
2019-07-17 18:39:09 +0900

    Update
    Update

---
+++ Cube/MoveCube.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject cube;
+    private Rigidbody rigidBody;
+
+    private Vector3 moveVector;
+
+    private void Start()
+    {
+        rigidBody = gameObject.GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            moveVector.z = 1;
+            StartCoroutine(Move());
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            moveVector.z = -1;
+            StartCoroutine(Move());
+
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            moveVector.x = 1;
+            StartCoroutine(Move());
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            moveVector.x = -1;
+            StartCoroutine(Move());
+
+        }
+
+    }
+
+    private IEnumerator Move()
+    {
+
+
+        for (int i = 0; i < 90; i++)
+        {
+            cube.transform.Rotate(moveVector,Space.World);
+            yield return new WaitForSeconds(0.01f);
+        }
+
+        moveVector = new Vector3(0,0,0);
+    }
+
+}
--- Manager/StageManager.cs
+++ Manager/StageManager.cs
@@ -10,27 +10,12 @@ public class StageManager : MonoBehaviour
 
     private void Start()
     {
-        stageCube = GameObject.FindWithTag("StageCube");
+
     }
 
     private void Update()
     {
-        if (GameManager.instance.touchManager.IsSwiped)
-            RotateCube();
+
     }
 
-    public void SetRotateSensitivity(Slider slider)
-    {
-        rotateSensitivity = slider.value;
-    }
-
-    private void RotateCube()
-    {
-        if (GameManager.instance.touchManager.SwipeDirection.x < 0)
-            stageCube.transform.Rotate(Vector3.up * Time.deltaTime * rotateSensitivity * 30);
-
-        else if (GameManager.instance.touchManager.SwipeDirection.x > 0)
-            stageCube.transform.Rotate(Vector3.up * -1 * Time.deltaTime * rotateSensitivity * 30);
-          
-    }
 }
